cce7eb5982e5bfa7f03cf11ada8546542123c255
Sascha_T <info@sascha-t.de>
2018-09-16 17:29:23 +0200

    Add Debian and RedHat builds to build.ts. Let the testing begin!
    Signed-off-by: Sascha_T <info@sascha-t.de>

--- build.ts
+++ build.ts
@@ -1,21 +1,53 @@
-let electronInstaller = require("electron-winstaller");
+let windowsInstaller = require("electron-winstaller");
+var linuxInstaller = require("electron-linux-installer");
 async function main() {
-  let resultPromise32 = electronInstaller.createWindowsInstaller({
-    appDirectory: "./builds/kukilauncher-win32-ia32",
-    outputDirectory: "./release/installer32",
-    authors: "KukiTeam",
-    description: "Launches KukiCraft",
-    exe: "kukilauncher.exe"
+  let resultPromise32 = windowsInstaller
+    .createWindowsInstaller({
+      appDirectory: "./builds/kukilauncher-win32-ia32",
+      outputDirectory: "./release/installer32",
+      authors: "KukiTeam",
+      description: "Launches KukiCraft",
+      exe: "kukilauncher.exe"
+    })
+    .then(success => {
+      console.log("Win32 Build done!");
+    });
+
+  let resultPromise64 = windowsInstaller
+    .createWindowsInstaller({
+      appDirectory: "./builds/kukilauncher-win32-x64",
+      outputDirectory: "./release/installer64",
+      authors: "KukiTeam",
+      description: "Launches KukiCraft",
+      exe: "kukilauncher.exe"
+    })
+    .then(success => {
+      console.log("Win64 Build done!");
+    });
+
+  let debianPromise = linuxInstaller({
+    src: "builds/kukilauncher-linux-x64/", // source location
+    dest: "./release/debian/", // destination of the installer
+    arch: "x86_64", // x86_x64 would work both debian and rpm cause controllers are here.
+    for: "debian" // can be debian or redhat
+  }).then(success => {
+    console.log("Debian Build done!");
   });
 
-  let resultPromise64 = electronInstaller.createWindowsInstaller({
-    appDirectory: "./builds/kukilauncher-win32-x64",
-    outputDirectory: "./release/installer64",
-    authors: "KukiTeam",
-    description: "Launches KukiCraft",
-    exe: "kukilauncher.exe"
+  let redhatPromise = linuxInstaller({
+    src: "builds/kukilauncher-linux-x64/", // source location
+    dest: "./release/redhat/", // destination of the installer
+    arch: "x86_64", // x86_x64 would work both debian and rpm cause controllers are here.
+    for: "redhat" // can be debian or redhat
+  }).then(success => {
+    console.log("RedHat Build done!");
   });
-  let promises = [resultPromise32, resultPromise64];
+  let promises = [
+    resultPromise32,
+    resultPromise64,
+    redhatPromise,
+    debianPromise
+  ];
   Promise.all(promises);
 }
 main();
